85cbf93471ea97ee83a20472406d2a55e3a959b5
Qiming Zhao <chemzqm@gmail.com>
2022-11-18 04:03:14 +0800

    better emoji completion, use startcol

--- packages/emoji/index.js
+++ packages/emoji/index.js
@@ -1,27 +1,27 @@
-const { sources, workspace } = require('coc.nvim')
-const path = require('path')
+const {sources} = require('coc.nvim')
 const fs = require('fs')
+const {promisify} = require('util')
 
-let items = []
+let items
 
 exports.activate = async context => {
-  let file = path.resolve(__dirname, 'emoji.txt')
-  if (!fs.existsSync(file)) return
-
-  fs.readFile(file, 'utf8', (err, content) => {
-    if (err) return
-    let lines = content.split(/\n/).slice(0, -1)
-    items = lines.map(str => {
-      let parts = str.split(':')
-      return { description: parts[0], character: parts[1] }
-    })
-  })
+  const filpeath = context.asAbsolutePath('emoji.txt')
 
   let source = {
     name: 'emoji',
     triggerOnly: true,
-    doComplete: async function () {
+    doComplete: async function (opt) {
+      if (!items) {
+        let content = await promisify(fs.readFile)(filpeath, 'utf8')
+        let lines = content.split(/\n/).slice(0, -1)
+        items = lines.map(str => {
+          let parts = str.split(':')
+          return {description: parts[0], character: parts[1]}
+        })
+      }
+
       return {
+        startcol: opt.col - 1,
         items: items.map(o => {
           return {
             word: o.character,
@@ -31,17 +31,7 @@ exports.activate = async context => {
           }
         })
       }
-    },
-    onCompleteDone: async (item, opt) => {
-      let { nvim } = workspace
-      let { linenr, col, input, line } = opt
-      let buf = Buffer.from(line, 'utf8')
-      let pre = buf.slice(0, col - 1).toString('utf8')
-      let after = buf.slice(col + input.length).toString('utf8')
-      await nvim.call('coc#util#setline', [linenr, `${pre}${item.word}${after}`])
-      await nvim.call('cursor', [linenr, Buffer.byteLength(`${pre}${item.word}`) + 1])
     }
   }
-
   context.subscriptions.push(sources.createSource(source))
 }
